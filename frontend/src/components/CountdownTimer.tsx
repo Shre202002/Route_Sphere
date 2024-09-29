f4019e61196853267bec4582c8d946049393cbff
Sriyansh Gupta <sriyanshgupta24@gmail.com>
2024-09-29 17:11:47 +0530

    resolved error for deployment

--- frontend/src/components/CountdownTimer.tsx
+++ frontend/src/components/CountdownTimer.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker-cssmodules.css"
 
 interface CountdownProps {
